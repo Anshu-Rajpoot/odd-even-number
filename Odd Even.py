213128098faeb3c49cc624d6fc60c6755036015e
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2023-03-12 00:05:14 +0530

    Add files via upload

---
+++ Odd Even.py
@@ -0,0 +1,7 @@
+number=int(input("Enter the Number : "))
+def odd_even(number):
+    if(number//2)==0:
+        print(number, "Number is Even")
+    else:
+        print(number,"Number is Odd")
+odd_even(number)
